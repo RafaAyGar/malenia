687527f660d3a0c6d78525d82c9d18170df80079
RafaAyGar <rafaaylloningeniero@gmail.com>
2023-03-08 19:26:59 +0100

    First push

---
+++ ml_lab/test.py
@@ -0,0 +1,5 @@
+
+
+
+def test():
+    print("Funciono correctamente!")
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from ml_lab import __version__
+
+setup(
+    name='ml_lab',
+    version=__version__,
+
+    url='https://github.com/RafaAyGar/ml-lab',
+    author='Rafael Ayllón Gavilán',
+    author_email='rafaaylloningeniero@gmail.com',
+
+    py_modules=['ml_lab'],
+)
