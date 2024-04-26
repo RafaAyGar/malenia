9bc0edafad7ab8b6404a40a49319398cf0d4e14a
RafaAyGar <rafaaylloningeniero@gmail.com>
2024-04-26 08:39:02 +0200

    added util get_datasets

--- malenia/launch_utils.py
+++ malenia/launch_utils.py
@@ -1,4 +1,7 @@
+import os
 from dataclasses import dataclass
+
+from malenia.dataset import Dataset
 
 
 @dataclass
@@ -10,3 +13,15 @@ class CondorParams:
     request_CPUs: int = 0
     request_GPUs: int = 0
     request_memory: str = "1G"
+
+
+def get_datasets(data_types_and_paths, limited_databases, excluded_databases):
+    datasets = []
+    for data_type, data_path in data_types_and_paths.items():
+        databases = os.listdir(data_path)
+        databases.sort()
+        for db in databases if limited_databases == "use_all" else limited_databases:
+            if db not in excluded_databases:
+                datasets.append(Dataset(name=db, dataset_type=data_type, path=data_path))
+
+    return datasets
